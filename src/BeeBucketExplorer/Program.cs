63ebf4233037d9a8661541695656fab696ea64fb
Mirko Da Corte <mirko@etherna.io>
2024-04-22 17:34:26 +0200

    Add Bee.Net package and implement postage batches retrieval

--- src/BeeBucketExplorer/Program.cs
+++ src/BeeBucketExplorer/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Etherna.BeeBucketExplorer;
 
-class Program
+sealed class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var beeUrl = args[0];
+
+        using var httpClient = new HttpClient();
+        var beeClient = new BeeNet.Clients.DebugApi.BeeDebugClient(new Uri(beeUrl, UriKind.Absolute), httpClient);
+        
+        // Get postage stamps.
+        var postageBatches = await beeClient.GetOwnedPostageBatchesByNodeAsync();
+
+        foreach (var postage in postageBatches)
+        {
+            var bucketBatch = await beeClient.GetStampsBucketsForBatchAsync(postage.Id);
+
+            Console.WriteLine($"postage Id: {postage.Id}, depth: {postage.Depth}");
+            
+            var maxCollisionsBucket = bucketBatch.Buckets.MaxBy(b => b.Collisions)!;
+            Console.WriteLine($"  max collisions bucket: {maxCollisionsBucket.Collisions}");
+        }
     }
 }
